3a589038d185a1b4cc8abde6a05495b84d80ceac
Александр Некрасов <lxmrrr@gmail.com>
2016-02-17 12:48:03 +0300

    стили слайдера, добавление слайдера на все страницы, доб. скрипта для открытия первой вкладки

--- js/script.js
+++ js/script.js
@@ -9,6 +9,50 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	
+	$(".mobile-menu__button").click(function(){
+		$(".mobile-menu__list").slideToggle();
+	});
+	$(".mobile-menu__list a").click(function(){
+		$(".mobile-menu__list").slideToggle();
+	});
+	
+	$("#link-1").click(function(){
+		$('a[href="#1"]').tab('show');
+	});
+	$("#link-2").click(function(){
+		$('a[href="#2"]').tab('show');
+	});
+	$("#link-3").click(function(){
+		$('a[href="#3"]').tab('show');
+	});
+	$("#link-4").click(function(){
+		$('a[href="#4"]').tab('show');
+	});
+	$("#link-5").click(function(){
+		$('a[href="#5"]').tab('show');
+	});
+	$("#link-6").click(function(){
+		$('a[href="#6"]').tab('show');
+	});	
+	
+	x = $(location).attr('href');
+	x = x.substr(x.indexOf('#')); 		
+	if (x != "l") {
+		$('a[href="' + x + '"]').tab('show');
+	} else {
+		$('div#1').addClass('in');
+	}
+});
+
+lightbox.option({
+	alwaysShowNavOnTouchDevices: true,
+	disableScrolling: true,
+	fadeDuration:400,
+	resizeDuration: 500,
+});
+
 {
 
     var myCenter=new google.maps.LatLng(54.212117, 37.646803);
